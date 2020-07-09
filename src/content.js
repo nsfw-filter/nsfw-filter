3a67854376effacae9de7504c7c486d85780ba73
Navendu Pottekkat <navendu@localhost.localdomain>
2020-07-09 13:28:18 +0530

    fixed loading issue

--- src/content.js
+++ src/content.js
@@ -1,17 +1,17 @@
 const DEBUG = 1;
-if (!DEBUG) console.log = () => {};
+if (!DEBUG) console.log = () => { };
 
 let isScrolling;
 let images = [...document.getElementsByTagName('img')];
 
 function clasifyImages() {
-  [...images, ...document.getElementsByTagName('img')].unique().forEach(analyzeImage);  
+  [...images, ...document.getElementsByTagName('img')].unique().forEach(analyzeImage);
 }
 
 function validImage(image) {
   const valid = image.src &&
-        image.width > 64 && image.height > 64 &&
-        !image.dataset.catReplaced;
+    image.width > 64 && image.height > 64 &&
+    !image.dataset.catReplaced;
   console.log('image %s valid', image.src, valid);
   return valid;
 }
@@ -19,7 +19,7 @@ function validImage(image) {
 function analyzeImage(image) {
   console.log('analyze image %s', image.src);
 
-  chrome.runtime.sendMessage({url: image.src}, response => {
+  chrome.runtime.sendMessage({ url: image.src }, response => {
     console.log('prediction for image %s', image.src, response);
     console.log(image);
     if (response && response.result === true) {
@@ -32,15 +32,19 @@ function analyzeImage(image) {
   });
 }
 
-document.addEventListener("scroll", (images)=>{ 
+document.addEventListener("scroll", (images) => {
   clearTimeout(isScrolling);
-  isScrolling = setTimeout(()=>{clasifyImages()}, 100);
+  isScrolling = setTimeout(() => { clasifyImages() }, 100);
 });
 
-Array.prototype.unique = function() {
-  return this.filter(function (value, index, self) { 
+Array.prototype.unique = function () {
+  return this.filter(function (value, index, self) {
     return self.indexOf(value) === index;
   });
 }
 
-clasifyImages();
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+  if (changeInfo.status == 'complete' && tab.active) {
+    clasifyImages();
+  }
+})
