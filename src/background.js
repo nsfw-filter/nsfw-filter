3ec72ae8736a60e8cf0c0a705cdd47ee5b46cce1
Navendu Pottekkat <navendu@localhost.localdomain>
2020-07-10 11:42:41 +0530

    fixed loading issue

--- src/background.js
+++ src/background.js
@@ -5,30 +5,16 @@ import * as nsfwjs from 'nsfwjs'
 
 tf.enableProdMode()
 
-// Original code, commented to test a few things, the below code works!
-
-// const img = document.getElementById('img')
-
-// Load model from my S3.
-// See the section hosting the model files on your site.
-
-// nsfwjs.load().then(function (model) {
-//   model.classify(img).then(function (predictions) {
-//     // Classify the image
-//     console.log('Predictions: ', predictions)
-//   })
-// })
-
-// End of the working code
-
 const IMAGE_SIZE = 224;
-const CAT_THRESHOLD = 0.9;
+const FILTER_THRESHOLD = 0.75;
+const FILTER_LIST = ["Hentai", "Porn", "Sexy"];
 
 nsfwjs.load('../models/').then(model => {
 
   async function loadImage(url) {
     const image = new Image(IMAGE_SIZE, IMAGE_SIZE);
     return new Promise((resolve, reject) => {
+      image.crossOrigin = "anonymous";
       image.onload = () => resolve(image);
       image.onerror = (err) => reject(err);
       image.src = url;
@@ -47,34 +33,36 @@ nsfwjs.load('../models/').then(model => {
 
     // MobileNet V1: model.predict(tensor)
     // MobileNet V2: model.predict({ Placeholder: tensor })
-    const prediction = model.classify(image);
+    const prediction = model.classify(image, 1);
 
-    const output = prediction; // [cat_probability, nocat_probability]
-    console.log('Prediction for %s', url, output);
+    const output = prediction;
+
+    // const output_test = prediction[0].className; // [cat_probability, nocat_probability]
+    // console.log('Prediction for %s', url, output);
+
+    // if (output_test == 'Neutral') {
+    //   console.log("Safe")
+    // }
 
     return output;
   }
 
   chrome.runtime.onMessage.addListener((request, sender, callback) => {
     executeModel(request.url)
-      .then(result => callback({ result: result[0] > CAT_THRESHOLD }))
+      .then(op => {
+        if (FILTER_LIST.includes(op[0].className)) {
+          console.log("EUREKA! EUREKA! EUREKA!")
+          console.log(op[0].className, op[0].probability)
+          return true;
+        }
+        else {
+          return false;
+        }
+      })
+      .then(result => callback({ result: result }))
       .catch(err => callback({ result: false, err: err.message }));
 
     return true; // needed to make the content script wait for the async processing to complete
   });
 
 });
-
-// Testing Script for the model
-
-// async function app () {
-
-// const model2 = await nsfwjs.load('../models/');
-// const img = document.getElementById('img')
-
-// const predictions = await model2.classify(img)
-// console.log('Predictions: ', predictions)
-
-// }
-
-// app();
