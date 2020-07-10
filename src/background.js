85b676ecac4d48d565c428d22c496de50364a684
Navendu Pottekkat <navendu@localhost.localdomain>
2020-07-10 14:14:14 +0530

    added comments

--- src/background.js
+++ src/background.js
@@ -1,17 +1,43 @@
+/**
+* @license
+* Copyright 2020 Navendu Pottekkat. All Rights Reserved.
+* Licensed under the GNU General Public License v3.0
+* License copy at https://github.com/navendu-pottekkat/nsfw-blocker/blob/master/LICENSE
+*
+* =======================================================================================
+*
+* Permissions of this strong copyleft license are conditioned on making 
+* available complete source code of licensed works and modifications, 
+* which include larger works using a licensed work, 
+* under the same license. Copyright and license notices must be preserved. 
+* Contributors provide an express grant of patent rights.
+*
+* =======================================================================================
+*/
+
 import 'regenerator-runtime/runtime'
 
 import * as tf from '@tensorflow/tfjs'
 import * as nsfwjs from 'nsfwjs'
 
+// Enable production mode in TensorFlow
+
 tf.enableProdMode()
 
-const IMAGE_SIZE = 224;
-const FILTER_THRESHOLD = 0.75;
-const FILTER_LIST = ["Hentai", "Porn", "Sexy"];
+const IMAGE_SIZE = 224; // nsfwjs model used here takes input tensors as 224x224
+const FILTER_THRESHOLD = 0.75; // you can set a threshold and use the filter only if the predictions are above the threshold
+const FILTER_LIST = ["Hentai", "Porn", "Sexy"]; // the image classes that needs to be filtered
+const MODEL_PATH = '../models/'; // the model is stored as a web accessible resource
 
-nsfwjs.load('../models/').then(model => {
-
+nsfwjs.load(MODEL_PATH).then(model => {
+  /*
+  This function loads the nsfwjs model to memory and prepares it for making predictions.
+  Once the model is loaded, it will load the images and make predictions.
+  */
   async function loadImage(url) {
+    /*
+    This function loads the image for passing to the model.
+    */
     const image = new Image(IMAGE_SIZE, IMAGE_SIZE);
     return new Promise((resolve, reject) => {
       image.crossOrigin = "anonymous";
@@ -22,28 +48,12 @@ nsfwjs.load('../models/').then(model => {
   }
 
   async function executeModel(url) {
+    /*
+    Executes the model and returns predicitions.
+    */
     const image = await loadImage(url);
-
-    // input pixels must be floats in (0,1) range instead of integers (0,255)
-    // const tensor = tf
-    //   .fromPixels(image)
-    //   .toFloat()
-    //   .div(255)
-    //   .reshape([1, IMAGE_SIZE, IMAGE_SIZE, 3]);
-
-    // MobileNet V1: model.predict(tensor)
-    // MobileNet V2: model.predict({ Placeholder: tensor })
-    const prediction = model.classify(image, 1);
-
+    const prediction = model.classify(image, 1); // Change the second parameter to change the number of top predicitions returned
     const output = prediction;
-
-    // const output_test = prediction[0].className; // [cat_probability, nocat_probability]
-    // console.log('Prediction for %s', url, output);
-
-    // if (output_test == 'Neutral') {
-    //   console.log("Safe")
-    // }
-
     return output;
   }
 
@@ -51,7 +61,9 @@ nsfwjs.load('../models/').then(model => {
     executeModel(request.url)
       .then(op => {
         if (FILTER_LIST.includes(op[0].className)) {
-          console.log("EUREKA! EUREKA! EUREKA!")
+          /*
+          If the top predicition is in our filter list, filter the image (return true)
+          */
           console.log(op[0].className, op[0].probability)
           return true;
         }
@@ -61,8 +73,6 @@ nsfwjs.load('../models/').then(model => {
       })
       .then(result => callback({ result: result }))
       .catch(err => callback({ result: false, err: err.message }));
-
     return true; // needed to make the content script wait for the async processing to complete
   });
-
 });
--- src/content.js
+++ src/content.js
@@ -1,38 +1,75 @@
+/**
+* @license
+* Copyright 2020 Navendu Pottekkat. All Rights Reserved.
+* Licensed under the GNU General Public License v3.0
+* License copy at https://github.com/navendu-pottekkat/nsfw-blocker/blob/master/LICENSE
+*
+* =======================================================================================
+*
+* Permissions of this strong copyleft license are conditioned on making 
+* available complete source code of licensed works and modifications, 
+* which include larger works using a licensed work, 
+* under the same license. Copyright and license notices must be preserved. 
+* Contributors provide an express grant of patent rights.
+*
+* =======================================================================================
+*/
+
+// Set DEBUG to 0 to prevent logging in the console.
+// Used for DEBUGGING purposes.
 const DEBUG = 1;
 if (!DEBUG) console.log = () => { };
 
+
+// The script is executed when a user scrolls through a website on the tab that is active in the browser.
 let isScrolling;
 let images = [...document.getElementsByTagName('img')];
 
 function clasifyImages() {
-  [...images, ...document.getElementsByTagName('img')].unique().forEach(analyzeImage);
+  /*
+  Classifies images and calls all the helper functions.
+  */
+  [...images, ...document.getElementsByTagName('img')].unique().filter(validImage).forEach(analyzeImage);
 }
 
 function validImage(image) {
+  /*
+  Checks if the image is of a certain height and width and check if the image has already been replaced,
+  preventing infinite loops.
+  */
   const valid = image.src &&
     image.width > 64 && image.height > 64 &&
-    !image.dataset.catReplaced;
+    !image.dataset.isReplaced;
   console.log('image %s valid', image.src, valid);
   return valid;
 }
 
 function analyzeImage(image) {
+  /*
+  Calls the background script passing it the image URL.
+  */
   console.log('analyze image %s', image.src);
-
   chrome.runtime.sendMessage({ url: image.src }, response => {
     console.log('prediction for image %s', image.src, response);
     console.log(image);
     if (response && response.result === true) {
+      /*
+      If the image should be filtered, it replaces the NSFW 
+      image with a random image from https://unsplash.com
+      */
       const replacedImageSrc = "https://source.unsplash.com/random/" + image.width + "x" + image.height;
       image.src = replacedImageSrc;
       image.srcset = "";
-      image.dataset.cat = true;
-      image.dataset.catReplaced = true;
+      image.dataset.filtered = true; // sets filterd to true so that they are not analyzed again
+      image.dataset.isReplaced = true; // sets isReplaced to true so that they are not analyzed again
     }
   });
 }
 
 document.addEventListener("scroll", (images) => {
+  /*
+  Call function when scrolling and timeout after scrolling stops.
+  */
   clearTimeout(isScrolling);
   isScrolling = setTimeout(() => { clasifyImages() }, 100);
 });
@@ -45,6 +82,9 @@ Array.prototype.unique = function () {
 
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
   if (changeInfo.status == 'complete' && tab.active) {
+    /*
+    Run classification on the active tab.
+    */
     clasifyImages();
   }
 })
