9998f6674ab0d81bd31d3b4c66ab8f6511f6df2e
Yegor <egorzaremba@gmail.com>
2020-07-26 13:15:24 +0300

    fix: lazy load and js render

--- src/content.js
+++ src/content.js
@@ -21,27 +21,23 @@
 const DEBUG = true;
 if (!DEBUG) console.log = () => {};
 
-const images = document.getElementsByTagName('img');
-
 function clasifyImages() {
-  // Hide every image until figure out its safety
+  const images = document.getElementsByTagName('img')
   for (let i = 0; i < images.length; i++) {
-    if (!images[i].__isPreChecked) {
-      images[i].__isPreChecked = true
-
-      images[i].style.visibility = 'hidden'
-    }
-  }
-
-  for (let i = 0; i < images.length; i++) {
-    if (!images[i].__isChecked) {
-      images[i].__isChecked = true
-
+    if (!(images[i].__isNSFW || images[i].__isChecked)) {
       if (images[i].src && images[i].width > 64 && images[i].height > 64) {
+        images[i].style.visibility = 'hidden'
+
         analyzeImage(images[i]);
-      } else {
-        images[i].style.visibility = 'visible'
+
+        // lazy load handler
+        const isImageLazyLoad = JSON.stringify([images[i].src, images[i].classList, images[i].className]).includes('lazy')
+        if (!isImageLazyLoad) {
+          images[i].__isChecked = true
+        }
       }
+
+      // @todo handle unsafe images smaller than 64px
     }
   }
 }
@@ -49,20 +45,30 @@ function clasifyImages() {
 // Calls the background script passing it the image URL
 function analyzeImage(image) {
   console.log('analyze image %s', image.src);
-  chrome.runtime.sendMessage({ url: image.src }, response => {
+  chrome.runtime.sendMessage({ url: image.src, url2: image.src }, response => {
+    console.log(`Prediction result is ${response ? response.result : 'undefined'} for image ${image.src}`);
     if (response && response.result === false) {
-      console.log(`Prediction result is ${response.result} for image ${image.src}`);
       image.style.visibility = 'visible'
+    } else {
+      image.__isNSFW = true
     }
   });
 }
 
 // Call function when DOM is loaded
-document.addEventListener("DOMContentLoaded", () => { clasifyImages() });
+window.addEventListener("load", () => {
+  clasifyImages()
+
+  // @todo hanlde with javascript render delay
+  const timesToJSrun = [500, 1000, 2000, 3000]
+  for (let i = 0; i < timesToJSrun.length; i++) {
+    setTimeout(() => { clasifyImages() }, timesToJSrun[i]);
+  }
+});
 
 // The script is executed when a user scrolls through a website on the tab that is active in the browser.
-let isScrolling;
 // Call function when the user scrolls because most pages lazy load the images
+let isScrolling;
 document.addEventListener("scroll", () => {
   clearTimeout(isScrolling);
   isScrolling = setTimeout(() => { clasifyImages() }, 100);
