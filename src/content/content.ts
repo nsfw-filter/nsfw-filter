9030d10957a16f003d29bff2529eb59e77c0629f
Yegor Zaremba <egorzaremba@gmail.com>
2020-08-26 21:01:46 +0300

    fix: remove popup

--- src/content/content.ts
+++ src/content/content.ts
@@ -1,6 +1,5 @@
 import { VideoFilter } from './Filter/VideoFilter'
 import { ImageFilter } from './Filter/ImageFilter'
-import { Store } from '../utils/Store'
 import { Memory } from '../utils/Memory'
 import { Logger } from '../utils/Logger'
 import { DOMWatcher } from './DOMWatcher/DOMWatcher'
@@ -12,18 +11,6 @@ const init = (): void => {
   const videoFilter = new VideoFilter(logger)
   const imageFilter = new ImageFilter(logger)
   new DOMWatcher(imageFilter, videoFilter).watch()
-
-  const store = new Store()
-  window.addEventListener('beforeunload', () => {
-    store.getCounters().then((result = { images: 0, videos: 0 }) => {
-      const updatedData = {
-        images: result.images + imageFilter.getBlockAmount(),
-        videos: result.videos + videoFilter.getBlockAmount()
-      }
-
-      store.saveCounters(updatedData).then(() => {}, () => {})
-    }, () => {})
-  })
 }
 
 // Ignore iframes, https://stackoverflow.com/a/326076/10432429
