c28518f48d4d3de29e9d99044fc91ac4eec23579
Yegor Zaremba <egorzaremba@gmail.com>
2020-10-24 11:48:26 +0300

    tmp

--- src/popup/components/Popup.tsx
+++ src/popup/components/Popup.tsx
@@ -1,14 +1,14 @@
 import React from 'react'
 
+import { Footer } from './Footer'
 import { Header } from './Header'
 import { Production } from './Production'
 import { Container } from './styles'
-import { Testing } from './Testing'
 
 export const Popup: React.FC = () => (
   <Container>
     <Header />
     <Production/>
-    <Testing />
+    <Footer />
   </Container>
 )
--- src/popup/redux/reducers/settings.ts
+++ src/popup/redux/reducers/settings.ts
@@ -2,7 +2,6 @@
 import { SettingsActionTypes } from '../actions/settings'
 import {
   TOGGLE_LOGGING,
-  TOGGLE_DIV_FILTERING,
   SET_FILTER_EFFECT,
   SET_TRAINED_MODEL,
   SET_FILTER_STRICTNESS
@@ -10,7 +9,6 @@ import {
 
 export type SettingsState = {
   logging: boolean
-  filteringDiv: boolean
   filterEffect: 'hide' | 'blur'
   trainedModel: 'mobile_v1.1'
   filterStrictness: number
@@ -18,18 +16,15 @@ export type SettingsState = {
 
 const initialState: SettingsState = {
   logging: false,
-  filteringDiv: false,
-  filterEffect: 'hide',
+  filterEffect: 'blur',
   trainedModel: 'mobile_v1.1',
-  filterStrictness: 90
+  filterStrictness: 80
 }
 
 export function settings (state = initialState, action: SettingsActionTypes): SettingsState {
   switch (action.type) {
     case TOGGLE_LOGGING:
       return { ...state, logging: !state.logging }
-    case TOGGLE_DIV_FILTERING:
-      return { ...state, filteringDiv: !state.filteringDiv }
     case SET_FILTER_EFFECT:
       return { ...state, filterEffect: action.payload.filterEffect }
     case SET_TRAINED_MODEL:
